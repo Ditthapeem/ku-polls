55bf117fd79b9238702be3b315b10ef91ec000fa
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-08-31 22:09:48 +0700

    Done part 2 Setup database and models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question
+
+# tell the admin that Question objects have an admin interface.
+
+admin.site.register(Question)
 
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,48 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    """A poll question with some choices."""
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        """Display a text in the question
+
+        Parameters:
+            self - the instance object
+        Return:
+            a string question
+        """
+        return self.question_text
+
+    def was_published_recently(self):
+        """Check that, Is this question was published recently.
+
+        Parameters:
+            self - the instance object
+        Return:
+            boolean
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    """An answer to a question."""
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """Display a text in the choice
+
+        Parameters:
+            self - the instance object
+        Return:
+            a string
+        """
+        return self.choice_text
+
 
 # Create your models here.
