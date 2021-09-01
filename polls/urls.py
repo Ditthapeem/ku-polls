e9dabc0bd29c1f0f2a5e738aac467ecf138e7a9d
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-09-01 17:09:04 +0700

    Done part 3 Writing more views

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,14 @@ from . import views
 # these url patterns are *relative* to 'polls' app,
 # so do not write the 'polls/' at beginning for the path
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from datetime import datetime
+from .models import Question
+
+
+# Create your views here.
+
 
 def index(request):
     """Display an index for this app.
@@ -10,6 +16,56 @@ def index(request):
     Return:
         an HttpResponse
     """
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list':latest_question_list,
 
-# Create your views here.
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    """Display an index of that question.
+
+    Parameters:
+            request - an HttpRequest containing the client's request data
+            question_id - an id of each question
+    Return:
+        an HttpResponse
+    """
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    """Display an index of the result of that question.
+
+    Parameters:
+            request - an HttpRequest containing the client's request data
+            question_id - an id of each question
+    Return:
+        an HttpResponse
+    """
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    """Display an index of question that you voted.
+
+    Parameters:
+            request - an HttpRequest containing the client's request data
+            question_id - an id of each question
+    Return:
+        an HttpResponse
+    """
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    try:
+        question = get_object_or_404(Question, pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
