2aa541e7cb985094597acb0c79f789803656d84a
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-09-14 16:45:35 +0700

    Show error message.

--- polls/views.py
+++ polls/views.py
@@ -1,39 +1,27 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from .models import Question, Choice
 from django.views import generic
 from django.utils import timezone
+from django.contrib import messages
 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
-# Create your views here.
-class IndexView(generic.ListView):
-    """A index page"""
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    """A detail page"""
-    model = Question
-    template_name = 'polls/detail.html'
-
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        can_vote_question = []
-        for question in Question.objects.all():
-            if question.can_vote() and question.is_published():
-                can_vote_question.append(question.id)
-        return Question.objects.filter(id__in=can_vote_question)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if question.can_vote() and question.is_published():
+        return render(request, 'polls/detail.html', {'question': question})
+    else:
+        messages.error(request, "Question is not available.")
+        return HttpResponseRedirect(reverse('polls:index'))
 
 
 class ResultsView(generic.DetailView):
