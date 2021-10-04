dc224dfc20224af00a0936489f203dfa6eb331e1
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-10-04 22:48:43 +0700

    Apply flake 8 to apps.py

--- polls/apps.py
+++ polls/apps.py
@@ -1,6 +1,9 @@
+"""This module is the configuration for polls app."""
 from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
+    """Configuration for polls app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
