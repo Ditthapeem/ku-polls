924c92188a8a98358b43a318773c33a35118af13
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-10-04 23:21:36 +0700

    Apply flake8 to admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,23 +1,30 @@
+"""This module is for admin interface."""
+
 from django.contrib import admin
 from .models import Question, Choice
 
-# tell the admin that Question objects have an admin interface.
-
 
 class ChoiceInline(admin.StackedInline):
+    """Tell the admin that Choice objects have an admin interface."""
+
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    """Tell the admin that Question objects have an admin interface."""
+
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}),
+        ('Date information',
+         {'fields': ['pub_date', 'end_date'],
+          'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
