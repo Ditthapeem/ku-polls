b98570166ef937c8c8aa77468ab5eb635d9b45ef
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-10-26 19:53:24 +0700

    #18 Create Sign in template

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup'),
     path('polls/', include('polls.urls'))
 ]
--- polls/views.py
+++ polls/views.py
@@ -9,6 +9,9 @@ from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -100,3 +103,20 @@ def get_vote_for_user(question, user):
             return vote[0]
     except Vote.DoesNotExist:
         return None
+
+def signup(request):
+    """Register a new user."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_passwd = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=raw_passwd)
+            login(request, user)
+            return redirect('polls')
+        # what if form is not valid?
+        # we should display a message in signup.html
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form':form})
