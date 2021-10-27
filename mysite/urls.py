3846f7b823c51529e567828613758ec1c77390b2
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-10-27 16:34:08 +0700

    Add link signin and login to each page.

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from polls import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('signup/', views.signup, name='register'),
+    path('signup/', views.signup, name='signup'),
     path('polls/', include('polls.urls'))
 ]
